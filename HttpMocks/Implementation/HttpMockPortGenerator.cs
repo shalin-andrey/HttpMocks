853e2f99a1cc5b881ff01ea25ef2e5033b5d47b5
shalin <shalin@skbkontur.ru>
2017-05-01 22:19:00 +0500

    generate port fix

--- HttpMocks/Implementation/HttpMockPortGenerator.cs
+++ HttpMocks/Implementation/HttpMockPortGenerator.cs
@@ -5,6 +5,7 @@ namespace HttpMocks.Implementation
 {
     internal class HttpMockPortGenerator
     {
+        private readonly IUnavailablePortsProvider unavailablePortsProvider;
         private readonly int minPortValue;
         private readonly int maxPortValue;
         private const int defaultMinPortValue = 20000;
@@ -15,29 +16,36 @@ namespace HttpMocks.Implementation
 
         internal HttpMockPortGenerator(IUnavailablePortsProvider unavailablePortsProvider, int minPortValue = defaultMinPortValue, int maxPortValue = defaultMaxPortValue)
         {
+            this.unavailablePortsProvider = unavailablePortsProvider;
             this.minPortValue = minPortValue;
             this.maxPortValue = maxPortValue;
-            actualPorts = new HashSet<int>(unavailablePortsProvider.GetUnavailablePorts());
+            actualPorts = new HashSet<int>();
             random = new Random(DateTime.Now.Millisecond);
         }
 
         public int GeneratePort()
         {
-            int newPort;
-            var tryCount = 0;
-            do
+            lock (actualPorts)
             {
-                newPort = random.Next(minPortValue, maxPortValue);
-                tryCount++;
-            } while (actualPorts.Contains(newPort) && tryCount < maxTryCount);
+                var unavailablePorts = unavailablePortsProvider.GetUnavailablePorts();
+                actualPorts.UnionWith(unavailablePorts);
 
-            if (tryCount >= maxTryCount)
-            {
-                throw new Exception($"Generate new port '{newPort}' error");
+                int newPort;
+                var tryCount = 0;
+                do
+                {
+                    newPort = random.Next(minPortValue, maxPortValue);
+                    tryCount++;
+                } while (actualPorts.Contains(newPort) && tryCount < maxTryCount);
+
+                if (tryCount >= maxTryCount)
+                {
+                    throw new Exception($"Generate new port '{newPort}' error");
+                }
+
+                actualPorts.Add(newPort);
+                return newPort;
             }
-
-            actualPorts.Add(newPort);
-            return newPort;
         }
     }
 }
